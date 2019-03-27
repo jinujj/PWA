570c6a48129939821001e79ec6c6543b102dd8d5
jinus303 <jinu.john.j@gmail.com>
2019-03-26 20:10:55 -0700

    added the PWA learning project

---
+++ news/app.js
@@ -0,0 +1,51 @@
+const apiKey = 'c7cfdbff3b1e4a559b0300de020271d0';
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'bbc-news';
+
+window.addEventListener('load', async e => {
+    updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    })
+
+    if('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('sw.js');
+            console.log('SW Registered');
+        } catch (error) {
+            console.log('SW Registration failed');
+        }
+    }
+});
+
+async function updateSources() {
+    const res = await fetch(`https://newsapi.org/v1/sources?apiKey=${apiKey}`);
+    const json = await res.json();
+
+    sourceSelector.innerHTML = json.sources
+    .map( src => `<option value="${src.id}" >${src.name}</option>`).join('\n');
+}
+
+
+async function updateNews(source = defaultSource)  {
+    const res = await fetch(`https://newsapi.org/v1/articles?source=${source}&apiKey=${apiKey}`);
+    const json = await res.json();
+
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+    return `
+    <div class="article">
+      <a href="${article.url}">
+        <h2>${article.title}</h2>
+        <img src="${article.urlToImage}" alt="${article.title}">
+        <p>${article.description}</p>
+      </a>
+    </div>
+  `;
+}
